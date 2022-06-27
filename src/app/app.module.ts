f1cc1c383916636c5de5dc70a9e4cec3bb1ed90d
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-06-27 12:47:19 -0300

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.component';
+import { FiltersComponent } from './components/bookstore-app/filters/filters.component';
+import { ProductListComponent } from './components/bookstore-app/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    BookstoreAppComponent,
+    FiltersComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
